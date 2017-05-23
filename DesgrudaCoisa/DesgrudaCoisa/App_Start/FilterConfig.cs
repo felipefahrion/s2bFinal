87871824a4066f1b9b2f06e83249fc0aef733a51
13109323 <13109323@W1847458>
2017-05-23 17:33:41 -0300

    Revert "Alteracao de nome de projeto | Inicio tela de Home"
    This reverts commit 37c7e2c9d7b8e2c93e9668ccece8a683a6e730a6.

---
+++ DesgrudaCoisa/DesgrudaCoisa/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DesgrudaCoisa
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DesgrudaCoisa/DesgrudaCoisa/Models/InicializadorBancoDeDados.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace DesgrudaCoisa.Models
+{
+    //public class InicializadorBancoDeDados : System.Data.Entity.DropCreateDatabaseIfModelChanges<MapeamentoEntidadesContext>
+    public class InicializadorBancoDeDados : System.Data.Entity.DropCreateDatabaseAlways<MapeamentoEntidadesContext>
+    {
+        protected override void Seed(MapeamentoEntidadesContext context)
+        {
+
+            var categorias = new List<Categoria>
+            {
+                new Categoria { TituloCategoria = "Informatica",  Descricao = "Produtos Informatica"},
+                new Categoria { TituloCategoria = "Estetica",  Descricao = "Produtos de Beleza"},
+
+             };
+            categorias.ForEach(s => context.Categorias.Add(s));
+            context.SaveChanges();
+
+
+            var anuncios = new List<Anuncio>
+            {
+                new Anuncio {
+                    TituloAnuncio = "Mouse",
+                    Valor = 10,
+                    DataPublicacao = DateTime.Parse("1/11/1989",new CultureInfo("en-US")),
+                    CategoriaID =  categorias.Single( g => g.TituloCategoria == "Informatica").CategoriaID,
+                },
+                new Anuncio {
+                    TituloAnuncio = "Shampo anti careca",
+                    Valor = 20,
+                    DataPublicacao = DateTime.Parse("1/11/2002",new CultureInfo("en-US")),
+                    CategoriaID =  categorias.Single( g => g.TituloCategoria == "Estetica").CategoriaID,
+                },
+             };
+            anuncios.ForEach(s => context.Anuncios.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
