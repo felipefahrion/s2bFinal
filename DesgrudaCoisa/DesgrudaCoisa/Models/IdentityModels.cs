e8738b5c740c5d66d20d8e8067df29b75fe63085
13109323 <13109323@W1846765>
2017-05-31 11:24:01 -0300

    Ajuste de Identity - Quando cria a tabela, cria as mesmas no mesmo contexto que está as outras tabelas

--- DesgrudaCoisa/DesgrudaCoisa/Models/IdentityModels.cs
+++ DesgrudaCoisa/DesgrudaCoisa/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace DesgrudaCoisa.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("InicializadorBD", throwIfV1Schema: false)
         {
         }
 
